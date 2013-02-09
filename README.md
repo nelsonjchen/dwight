ca9314b40c7c0d69f41a7ddae8cc4e342d0c773a
Nelson Chen <crazysim@gmail.com>
2013-02-09 15:17:29 -0800

    made playpause

---
+++ README.md
@@ -0,0 +1,11 @@
+# Playpause for Hermes on OS X
+
+Freaking waste of a framework that's what! This is a simple app to play and
+pause Hermes for OS X, a native OS X Pandora client (Not the official shit AIR
+version).
+
+Visit `/playpause` to playpause Hermes.
+
+This project was meant to be exported as a JAR file that you can just run on
+a Mac with Java and Hermes. There's no security and it's best for a LAN where
+everybody knows each other's names.
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,20 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import javax.script.{ScriptContext, ScriptEngineManager}
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def playpause = Action {
+    val runtime = Runtime.getRuntime
+    val args = Array("osascript", "-e", "tell application \"Hermes\" to playpause")
+    runtime.exec(args)
+
+    Ok("playpause")
   }
   
 }
